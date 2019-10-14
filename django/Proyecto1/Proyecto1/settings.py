6bda45b5d9264de68048c141c99b702c7eeda9b7
sslavik <sslavikshy@gmail.com>
2019-10-14 22:10:05 +0200

    Django template for /personas added

--- django/Proyecto1/Proyecto1/settings.py
+++ django/Proyecto1/Proyecto1/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Proyecto1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/home/usuario/SGEMP/django/Proyecto1/Proyecto1/views'],
+        'DIRS': ['/home/usuario/SGEMP/django/Proyecto1/Proyecto1/views', '/home/hacka/SGEMP/django/Proyecto1/Proyecto1/views'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- django/Proyecto1/Proyecto1/view.py
+++ django/Proyecto1/Proyecto1/view.py
@@ -4,8 +4,11 @@ from Proyecto1.models.persona import Persona
 
 
 def listaPersonas(request):
-    p1 = Persona("Persona 1", "Carrasco", 20, "Administración")
+    p1 = Persona("Jose Carlos", "Soler", 21, "Marketing")
+    p2 = Persona("Sergio", "Montel", 22, "RRHH")
+    p3 = Persona("Ismael", "Carrasco", 21, "Administración")
+    p4 = Persona("Pablo", "Torres", 24, "Producción")
 
-    personas = [p1]
+    personas = [p1,p2,p3,p4]
 
     return render(request, "personas.html", {"personas": personas})
