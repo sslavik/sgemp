7fcd229dd385e396b58ee6f79a72ab868c22c696
sslavik <sslavikshy@gmail.com>
2019-11-26 08:49:04 +0100

    added _generated copy from Original GoodStampa.com

---
+++ Vyacheslav_WEB/oficial/cms/index.js
@@ -0,0 +1,153 @@
+({
+	head: {
+		titulo: 'Good Stampa | Digitalizate de forma sencilla con nosotros',
+		new_css: [
+			{
+				$href_type: 'file',
+				href: ''
+			}
+		]
+	},
+	sidebar: {
+		marcador_1: 'Inicio',
+		marcador_2: 'Servicios',
+		marcador_3: '¿Cómo Funcionamos?',
+		marcador_4: 'Contacto'
+	},
+	inicio: {
+		titulo: 'GOOD STAMPA',
+		descripcion: 'Tu marca más visible'
+	},
+	quien: {
+		titulo_Quien_Somos: 'GOOD STAMPA ?',
+		descripcion: 'El mundo digital avanza a pasos agigantados. Por ello, nosotros desarrollamos una estrategia con la cual estaremos actualizados día a día con esta plataforma',
+		code: 'Desarrollo Web / App',
+		des_code: 'Nos amoldamos a las necesidades de su negocio con nuestra alta capacidad en la programación',
+		confidencial: 'Seguridad',
+		des_confidencial: 'Nos comprometemos a la protección y confidencialidad de sus datos',
+		config: 'Configuración RRSS',
+		des_config: 'Organización y planificación de su negocio en las Redes Sociales',
+		escritorio: 'Estadísticas',
+		des_escritorio: 'Seguimiento en tiempo real de sus Redes Sociales, Página web y App',
+		cadena: 'Fidelización',
+		des_cadena: 'Aseguramos la satisfacción de los clientes además de la expansión de su negocio',
+		diamante: 'Servicio',
+		des_diamante: 'Ofrecemos un trabajo profesional a la altura de su negocio y aprovechando al máximo nuestros recursos ',
+		more_info: 'Servicios'
+	},
+	servicios: {
+		nombre_extras: 'Complementos',
+		plata: 'Redes Sociales',
+		des_plata: [
+			{
+				describe: 'Creación y Preservación de RRSS'
+			},
+			{
+				describe: 'Organización  de  Ofertas, Sorteos y Eventos'
+			},
+			{
+				describe: 'Mínimo 2 publicaciones / Semanales'
+			},
+			{
+				describe: 'Aumento Seguidores e Impresiones'
+			},
+			{
+				describe: 'Fidelizar Clientes'
+			}
+		],
+		extra_plata: [
+			{
+				extra: 'Sesión fotográfica de su Negocio / Empresa'
+			},
+			{
+				extra: 'Promociones de publicaciones'
+			},
+			{
+				extra: 'Diseño Logotipo'
+			},
+			{
+				extra: 'Propuesta de ofertas'
+			},
+			{
+				extra: 'Posibilidad de incrementar promoción en publicaciones'
+			},
+			{
+				extra: 'Aumento Interacción'
+			}
+		],
+		oro: 'Redes Sociales + Página Web',
+		des_oro: [
+			{
+				describe: 'Creación de Página web'
+			},
+			{
+				describe: '1 Año Mantenimiento Gratis'
+			},
+			{
+				describe: 'Aumentar valor de su empresa en Internet'
+			},
+			{
+				describe: 'Posicionamiento SEO'
+			},
+			{
+				describe: 'E-Commerce adaptado a su negocio / empresa'
+			}
+		],
+		extra_oro: [
+			{
+				extra: 'Un mínimo de 4 Sub-Paginas'
+			},
+			{
+				extra: ''
+			}
+		],
+		platino: 'Redes Sociales + Página web + App',
+		des_platino: [
+			{
+				describe: 'Creación de App Web / Android & iOs'
+			},
+			{
+				describe: 'E-Commerce Con App'
+			},
+			{
+				describe: 'Promociones directamente a su cliente'
+			},
+			{
+				describe: 'Facilidad de acceso para los clientes'
+			},
+			{
+				describe: 'Totalmente personalizada para su negocio / empresa'
+			}
+		],
+		extra_platino: [
+			{
+				extra: 'Notificar a lo clientes de cambio de horario'
+			},
+			{
+				extra: 'Interacciones del cliente'
+			}
+		]
+	},
+	contacto: {
+		contacto: 'Contacto',
+		des_contacto: 'Puedes contactar con nosotros por cualquier medio. Te solucionaremos tu duda de inmediato.',
+		nombre: 'Nombre',
+		email: 'Correo',
+		des_email: 'goodstampa.ofi@gmail.com',
+		mensaje: 'Mensanje',
+		send: 'Enviar Mensaje',
+		direccion: 'Dirección',
+		tlf: 'Telefono',
+		des_tlf: '+34 640 26 10 77',
+		RRSS: 'Redes Sociales',
+		link_twitter: '',
+		link_facebook: 'https://fb.me/GoodStampa.Red',
+		link_instagram: ''
+	},
+	scripts: [
+		{
+			$script_type: 'file',
+			script: ''
+		}
+	]
+})
