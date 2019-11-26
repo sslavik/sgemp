a54246be6ad70588521975c6f8ef56260a23f6bd
sslavik <sslavikshy@gmail.com>
2019-11-26 10:28:35 +0100

    Added Web Page Official to views but not Working

--- Vyacheslav_WEB/Vyacheslav_WEB/settings.py
+++ Vyacheslav_WEB/Vyacheslav_WEB/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -62,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
             ],
         },
     },
@@ -103,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
--- Vyacheslav_WEB/Vyacheslav_WEB/urls.py
+++ Vyacheslav_WEB/Vyacheslav_WEB/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views  # Importamos las vistas de CORE Aquí
 
 urlpatterns = [
+    # Definimos donde se va a mostrar nuestra vista en la URL
+    path('', views.index, name="index"),
     path('admin/', admin.site.urls),
 ]
